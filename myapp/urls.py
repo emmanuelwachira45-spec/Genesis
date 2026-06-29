5dbe8dd66790b7a9d242a329ccde07ddf4d9feb2
emmanuelwachira45-spec <emmanuelwachira45@gmail.com>
2026-06-29 11:46:55 +0300

    initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('details/', views.details, name='details'),
+    path('blog/', views.blog, name='blog'),
+    path('', views.home, name='home'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('services/', views.services, name='services'),
+    path('starter/', views.starter, name='starter'),
+]
