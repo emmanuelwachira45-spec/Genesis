bc73e78927215318c98e4a95f91ac24162f34384
emmanuelwachira45-spec <emmanuelwachira45@gmail.com>
2026-07-01 11:51:39 +0300

    add new files

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
+from myapp.models import *
 
 # Create your views here.
 def details(request):
     return render(request, 'blog-details.html')
 
 def blog(request):
-    return render(request, 'blog.html')
+   return render(request, 'blog.html')
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        mycontact = Contact(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            subject = request.POST['subject'],
+            message = request.POST['message'] 
+        )
+        mycontact.save()
+        return render(request, 'index.html')
+    
+    else:
+        return render(request, 'index.html')
+    
 
 def portfolio(request):
     return render(request, 'portfolio-details.html')
